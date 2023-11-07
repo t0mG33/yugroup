21f84167a6b15fa86fa2201976e1cd92eb258705
t0mG33 <tgalmiche86@gmail.com>
2023-11-07 08:43:54 -0500

    Updated input form

--- index.php
+++ index.php
@@ -126,8 +126,8 @@
                         <input type="number" class="form-control" id='odm-read' name="vehichle_reading" placeholder='Enter odometer reading' required>
                     </div>
                     <div class="col-4 col-md-5">
-                        <label class="form-label text-start w-100" for="selling-price">Retail Selling Price</label>
-                        <input type="number" class="form-control" id="selling-price" name="retail_selling_price" placeholder="Enter retail selling price" required>
+                        <label class="form-label text-start w-100" for="retail_selling_price">Retail Selling Price</label>
+                        <input type="number" class="form-control" id="retail_selling_price" name="retail_selling_price" placeholder="Enter retail selling price" required>
                     </div>
                 </div>
             
@@ -159,59 +159,99 @@
                 <div class="row my-3">
                     <div class="col-2">
                         <label class="form-label text-start w-100" for="ti-year">Year</label>
-                        <input id="ti-year" class="form-control" type="number" name="tradein_year" placeholder="Enter trade-in's year" required>
+                        <input id="ti-year" class="form-control" type="number" name="tradein_year" placeholder="Enter trade-in's year">
                     </div>
                     <div class="col-3 col-md-3">
                         <label class="form-label text-start w-100" for="ti-make">Make</label>
-                        <input id="ti-make" class="form-control" type="text" name="tradein_make" placeholder="Enter trade-in's make" required>
+                        <input id="ti-make" class="form-control" type="text" name="tradein_make" placeholder="Enter trade-in's make">
                     </div>
                     <div class="col-7 col-md-3">
                         <label class="form-label text-start w-100" for="ti-model">Model</label>
-                        <input id="ti-model" class="form-control" type="text" name="tradein_model" placeholder="Enter trade-in's model" required>
+                        <input id="ti-model" class="form-control" type="text" name="tradein_model" placeholder="Enter trade-in's model" >
                     </div>
                     <div class="col-md-4">
                         <label class="form-label text-start w-100" for="ti-color">Colour</label>
-                        <input type="text" class="form-control" id='ti-color' name="tradein_colour" placeholder="Enter the trade-in's colour" required>
+                        <input type="text" class="form-control" id='ti-color' name="tradein_colour" placeholder="Enter the trade-in's colour">
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-6">
                         <label class="form-label text-start w-100" for="ti-model">Serial</label>
-                        <input id="ti-model" class="form-control" type="text" name="tradein_serial" placeholder="Enter trade-in's model" required>
+                        <input id="ti-model" class="form-control" type="text" name="tradein_serial" placeholder="Enter trade-in's model">
                     </div>
 
                     <div class="col-md-3">
                         <label class="form-label text-start w-100" for="odm-unit">Odometer unit</label>
                         <div class="checkbox-container text-start">
                             <div class="checkbox-group w-49 d-inline-block px-3">
-                                <input type="radio" class="form-check-input" id="ti-mile-unit" name="tradein_unit" value="mi" required>
+                                <input type="radio" class="form-check-input" id="ti-mile-unit" name="tradein_unit" value="mi">
                                 <label for="ti-mile-unit">MI</label>
                             </div>
                             <div class="checkbox-group w-49 d-inline-block">
-                                <input type="radio" class="form-check-input" id="ti-km-unit" name="tradein_unit" value="km" required>
+                                <input type="radio" class="form-check-input" id="ti-km-unit" name="tradein_unit" value="km">
                                 <label for="ti-km-unit">KM</label>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-3">
                         <label class="form-label text-start w-100" for="ti-odm-read">Odometer reading</label>
-                        <input type="number" class="form-control" id='ti-odm-read' name="tradein_reading" placeholder="Enter trade-in's odometer reading" required>
+                        <input type="number" class="form-control" id='ti-odm-read' name="tradein_reading" placeholder="Enter trade-in's odometer reading">
                     </div>
                 </div>
                 
                 <div class="row mt-4">
                     <div class="col-md-3">
                         <label class="form-label text-start w-100" for="ti-lien">Lien payable to</label>
-                        <input type="text" class="form-control" id='ti-lien' name="tradein_lien_payable" placeholder="Enter the name of lien creditor" required>
+                        <input type="text" class="form-control" id='ti-lien' name="tradein_lien_payable" placeholder="Enter the name of lien creditor">
                     </div>
                     <div class="col-md-7">
                         <label class="form-label text-start w-100" for="ti_lien-creditor-address">Address</label>
-                        <input type="text" class="form-control" id='ti_lien-creditor-address' name="tradein_address" placeholder="Enter the address of the lien creditor" required>
+                        <input type="text" class="form-control" id='ti_lien-creditor-address' name="tradein_address" placeholder="Enter the address of the lien creditor">
                     </div>
                     <div class="col-md-2">
-                        <label class="form-label text-start w-100" for="ti_lien-amount">Lien amount</label>
-                        <input type="number" class="form-control" id='ti_lien-amount' name="tradein_lien_amount" placeholder="Enter the amount due for the lien on the trade vehicle" required>
+                        <label class="form-label text-start w-100" for="tradein_lien_amount">Lien amount</label>
+                        <input type="number" class="form-control" id='tradein_lien_amount' name="tradein_lien_amount" placeholder="Enter the amount due for the lien on the trade vehicle">
+                    </div>
+                </div>
+
+            </fieldset>
+
+            <!-- Final terms of settlement -->
+            <fieldset class="border p-2 my-4">
+                <legend>Final terms of settlement</legend>
+                
+                <div class="row my-3">
+                    <div class="col-6 col-md-2">
+                        <label class="form-label text-start w-100" for="admin_fee">Admin. fee</label>
+                        <input id="admin_fee" class="form-control" type="number" name="admin_fee" placeholder="Enter amount">
+                    </div>
+                    <div class="col-6 col-md-2">
+                        <label class="form-label text-start w-100" for="finance_fee">Finance fee</label>
+                        <input id="finance_fee" class="form-control" type="number" name="finance_fee" placeholder="Enter amount">
+                    </div>
+                    <div class="col-6 col-md-2">
+                        <label class="form-label text-start w-100" for="sec_trans">Security trans.</label>
+                        <input id="sec_trans" class="form-control" type="number" name="sec_trans" placeholder="Enter amount" >
+                    </div>
+                    <div class="col-6 col-md-2">
+                        <label class="form-label text-start w-100" for="registration_fee">Reg./PPSA</label>
+                        <input type="number" class="form-control" id='registration_fee' name="registration_fee" placeholder="Enter amount">
+                    </div>
+                    <div class="col-12 col-md-2">
+                        <label class="form-label text-start w-100" for="eextended_service_contract">Ext. service contract</label>
+                        <input type="number" class="form-control" id='extended_service_contract' name="extended_service_contract" placeholder="Enter amount">
+                    </div>
+                    <div class="col-6 col-md-2">
+                        <label class="form-label text-start w-100" for="downpayment">Downpayment</label>
+                        <input id="downpayment" class="form-control" type="number" name="downpayment" placeholder="Enter amount">
+                    </div>
+                </div>
+
+                <div class="row my-3">
+                    <div class="col-12 col-md-2">
+                        <label class="form-label text-start w-100" for="payout_lien">Payout lien on trade</label>
+                        <input id="payout_lien" class="form-control" type="number" name="payout_lien" placeholder="Enter amount">
                     </div>
                 </div>
 
